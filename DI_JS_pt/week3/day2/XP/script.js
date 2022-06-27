d9d771e28cf2ca86d3992c984a78834eea6cb4ce
echugreeva <e.chugreeva@gmail.com>
2022-06-27 16:34:34 +0300

    w3d2 xpGold

--- DI_JS_pt/week3/day2/XP/script.js
+++ DI_JS_pt/week3/day2/XP/script.js
@@ -56,20 +56,146 @@ let b = 21;
 console.log(a+b);
  //first expression
 // Prediction: 55
-// Actual:
+// Actual: 55
 
 a = 2;
 
 console.log(a+b); 
 //second expression
 // Prediction: 23
-// Actual:
+// Actual: 23
 
 //What is the value of c?
 
 //prediction: undefined
+// Actual: undefined
+
 
 console.log(3 + 4 + '5');
 
-// Prediction: 23
+// Prediction: 75
 // Actual:75
+
+//ex 5    I put v in actual if the answer was same as prediction
+
+typeof(15)
+// Prediction: number
+// Actual:v 
+
+typeof(5.5)
+// Prediction: number
+// Actual:v
+
+typeof(NaN)
+// Prediction: number
+// Actual: v
+
+typeof("hello")
+// Prediction: string
+// Actual: v
+
+typeof(true)
+// Prediction: bollean
+// Actual: v
+
+typeof(1 != 2)
+// Prediction: bollean
+// Actual:v
+
+"hamburger" + "s"
+// Prediction: "hamburgers"
+// Actual: v
+
+"hamburgers" - "s"
+// Prediction: error
+// Actual: NaN
+
+"1" + "3"
+// Prediction: 13
+// Actual: '13' +-
+
+"1" - "3"
+// Prediction: error
+// Actual: -2
+
+"johnny" + 5
+// Prediction: johnny5
+// Actual: v
+
+"johnny" - 5
+// Prediction: error
+// Actual: NaN
+
+99 * "hello"
+// Prediction: "hello"
+// Actual: Uncaught SyntaxError: Unexpected token '*'
+
+1 != 1
+// Prediction: false
+// Actual: v
+
+1 == "1" 
+// Prediction: true
+// Actual: v
+
+1 === "1"
+// Prediction: false
+// Actual: v
+
+// ex 6--------------------------------------------I put v in actual if the answer was same as prediction
+
+5 + "34"
+// Prediction: '534'
+// Actual:v
+
+5 - "4"
+// Prediction: NaN
+// Actual: 1
+
+10 % 5
+// Prediction: 0
+// Actual:v
+
+5 % 10
+// Prediction: 1
+// Actual: 5
+
+"Java" + "Script"
+// Prediction: 'JavaScript'
+// Actual: v
+
+" " + " "
+// Prediction:"  "
+// Actual: v
+
+" " + 0
+// Prediction: " 0"
+// Actual:
+
+true + true
+// Prediction: true
+// Actual: 2
+
+true + false
+// Prediction: false / after true+true check : 1
+// Actual: 1
+
+false + true
+// Prediction: false  / after true+true check : 1
+// Actual: 1
+
+false - true
+// Prediction: NaN / after true+true check : -1
+// Actual: -1
+
+!true
+// Prediction: false
+// Actual: v
+
+3 - 4
+// Prediction: -1
+// Actual: v
+
+"Bob" - "bill"
+// Prediction: NaN
+// Actual: v
---
+++ DI_JS_pt/week3/day2/XPgold/script.js
@@ -0,0 +1,37 @@
+// ex 1
+
+let sentence = ["my","favorite","color","is","blue"];
+
+console.log(sentence.join(" "));
+
+
+//ex 2
+
+let str1="big";
+let str2="mistake";
+
+str2New = str1.slice(0,2);
+str1New =str2.slice(0,2);
+
+console.log(`${str2New} and ${str1}`);
+
+str1 = str1.replace("bi",str1New );
+
+console.log(str1);
+
+str2 = str2.replace("mi",str2New);
+
+var str3 = str1.concat(" ",str2);
+
+console.log(str3);
+
+
+//ex 3
+
+let num1=prompt("give you first number");
+var num2=prompt("give you secind number");
+
+num1=Number(num1);
+num2=Number(num2);
+
+alert(num1+num2);
