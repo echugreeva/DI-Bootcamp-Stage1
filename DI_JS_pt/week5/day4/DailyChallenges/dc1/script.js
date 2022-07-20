7bcd05173f4ccd0dc29db82f7bdafd906b54d066
echugreeva <e.chugreeva@gmail.com>
2022-07-20 16:34:04 +0300

    w5d4 DC 1

--- DI_JS_pt/week5/day4/DailyChallenges/dc1/script.js
+++ DI_JS_pt/week5/day4/DailyChallenges/dc1/script.js
@@ -1,37 +1,75 @@
-let color = null;
-let mousedown = false;
+// Create a function called addTask(). As soon as the user clicks on the button:
+// check that the input is not empty,
+// then add it to the array (ie. add the text of the task)
+// then add it to the DOM, below the form (in the <div class="listTasks"></div>) .
+// Each new task added should have (starting from left to right - check out the image at the top of the page)
+// a “X” button. Use font awesome for the “X” button.
+// an input type="checkbox". The label of the input is the task added by the user.
 
-let body = document.getElementsByTagName("body")[0];
-let colorsPicks = document.querySelectorAll(".colorPick");
-let fillColors = document.querySelectorAll(".toColor");
-let resetColor = document.getElementsByTagName("button")[0];
+// BONUS I (not mandatory):
+// Change the variable listTasks to an array of task objects.
+// Each new task added to the array should have the properties : task_id, text and done (a boolean - false by default).
+// Every new task object should have a task_id, starting from 0, and a data-task-id attribute, which value is the same as the task_id. Check out data-* attributes here.
+// Create a function named doneTask(), that as soon as the user clicks on the “checkbox” input, the done property should change from false to true in the object, and from black to crossed out red in the DOM.
 
-resetColor.addEventListener("click", function(){
-    for (fillColor of fillColors){
-        fillColor.style.backgroundColor = "white";
-    }
+// BONUS II (not mandatory):
+// Create a function named deleteTask(), that as soon as the user clicks on the “X” button, delete that specific task from the array listTasks.
+
+listTasks =[];
+let form = document.forms[0];
+let ul = document.querySelector("ul");
+let task; 
+let newLi;
+let xBtn;
+let checkbox;
+let CBLabel;
+let button = document.querySelector("#submit");
+
+
+let addTask = () => {
+    newLi = document.createElement("li");
+    listTasks.push(task);
+    task = form.elements.task.value;
+    if (task.length > 0) {
+        xBtn = document.createElement('i');
+        xBtn.classList.add("fa", "fa-thin", "fa-xmark");
+        checkbox = document.createElement('input');
+        checkbox.setAttribute('type', 'checkbox'); 
+        checkbox.setAttribute('name', 'task')
+        CBLabel = document.createElement('label');
+        CBLabel.setAttribute("for","task");
+        CBLabel.appendChild(document.createTextNode(task));
+        newLi.appendChild(xBtn);
+        newLi.appendChild(checkbox);
+        newLi.appendChild(CBLabel);
+        //newLi.appendChild(document.createTextNode(task));
+        ul.appendChild(newLi);
+        form.elements.task.value="";
+    } else { alert(`input task`)}
+    
+
+}
+
+//form submit didn't work
+
+// form.addEventListener("sumbit", function (){
+//     //event.preventDefault();
+//     addTask()
+// });
+
+
+button.addEventListener("click", function (event){
+    event.preventDefault();
+    console.log (`click btn`);
+    addTask();
 });
 
-body.addEventListener("mousedown", function(){
-    mousedown = true;
-})
 
-body.addEventListener("mouseup", function(){
-    mousedown = false;
-})
+// let task ={
+//     text: null,
+//     id:0,
+//     done: false
+// };
 
-
-for (colorsPick of colorsPicks){
-    colorsPick.addEventListener("click", function(event){
-        color = event.target.style.backgroundColor;
-    });
-}
-
-for (fillColor of fillColors){
-    fillColor.addEventListener("mousedown", function(event){
-        if (color != null) event.target.style.backgroundColor = color;
-    });
-    fillColor.addEventListener("mouseover", function(event){
-        if (mousedown && color != null) event.target.style.backgroundColor = color;
-    });
-}
+//task["text"] = form.element.input.value;
+//task['id']=id+1
