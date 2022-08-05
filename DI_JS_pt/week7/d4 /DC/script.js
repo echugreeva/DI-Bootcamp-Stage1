b28e375fd9288ad307a223974ee68ed45414e4e2
echugreeva <e.chugreeva@gmail.com>
2022-08-05 20:46:19 +0300

    w7d5

--- DI_JS_pt/week7/d4 /DC/script.js
+++ DI_JS_pt/week7/d4 /DC/script.js
@@ -28,11 +28,31 @@ class Video {
  let v = new Video ('cute cats', 'Willy Wonka', 180);
  v.watch();
 
- let v2 = new Video ('cute cats', 'Willy Wonka', 180);
+ let v2 = new Video ('future of galaxy', 'lord', 200);
  v2.watch();
 
-
+ 
  let videos = [
+   {title:'future of galaxy',
+    uploader:'lord',
+    time:200
+   },
+   {title:'the most epic movie',
+    uploader:'the best director',
+    time:20000
+   },
+   {title:'last week tonight',
+    uploader:'john oliver',
+    time:5000
+   },
+   {title:'all you need to know about dogs',
+    uploader:'Nancy',
+    time:7500
+   }
+  ];
 
-  
- ];
+  videos.forEach((e,i)=> {
+    let vs= ['v1','v2','v3', 'v4'];
+     vs[i]= new Video (e.title, e.uploader, e.time);
+    console.log (vs[i]);
+  })
