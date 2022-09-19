4bafca82375db8ba515f013f426946d79a162da5
echugreeva <e.chugreeva@gmail.com>
2022-09-19 19:47:55 +0300

    w13 d3-4

--- DI_JS_pt/week13/day3_4/server.js
+++ DI_JS_pt/week13/day3_4/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 app.use(cors());
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(bodyParser.json());
 app.use('/', express.static(path.join(__dirname)));
 
 
@@ -38,20 +38,63 @@ app.use('/', express.static(__dirname+'/public'))
 //     // in the root directory.
 //     res.render('home'); //renders the home.ejs file
 // })
+let data=[];
+// let dataToShow=[];
 let feed;
 app.get('/', (req,res)=>{
     (async () => {
 
         feed = await parser.parseURL('https://www.thefactsite.com/feed/');
-        // console.log(feed.title);
-      
-        feed.items.forEach(item => {
-        //   console.log(item.title + ':' + item.link)
-          console.log(item);})
-        })();
+        console.log(`feed length ${feed.items.length}`)
+        data = feed.items;
+        console.log(typeof(feed));
+        // for (let i = 0; i < data.length; i++ ){
+        //     data.push(data[i])
+        // }
+    //    console.log(feed);
+    console.log(`data array length ${data.length} `)
+    console.log(data[0])   ;
     res.render('public/index',{
-      title:'homepage',
-      data:feed
+      data:data
     })
-  })
-  
+})()
+})
+app.get('/search', (req,res)=>{
+    (async () => {
+
+        feed = await parser.parseURL('https://www.thefactsite.com/feed/');
+        console.log(`feed length ${feed.items.length}`)
+        data = feed.items;
+        console.log(typeof(feed));
+        // for (let i = 0; i < data.length; i++ ){
+        //     data.push(data[i])
+        // }
+    //    console.log(feed);
+    console.log(`data array length ${data.length} `)
+    console.log(data[0])   ;
+    res.render('public/search',{
+      data:data })
+    })()
+})
+
+app.post('/search',(req, res)=>{
+    console.log(`this is ${req.body}`);
+    console.log(typeof(req.body))
+    // let filter=JSON.parse(req.body)
+    // console.log(filter)
+    let cat = req.body.cat;
+    console.log(cat)
+    let title = req.body.title;
+    (async () => {
+
+        feed = await parser.parseURL('https://www.thefactsite.com/feed/');
+        console.log(`feed length ${feed.items.length}`)
+        data = feed.items;
+        
+        console.log(typeof(feed));
+        console.log(typeof(data));
+        data = data.filter((item=>{item.creator.includes(cat)}))
+    res.render('public/search',{
+      data:data })
+    })()
+})
