023571f129280817e81ad4577f5951ca13c4a7d4
echugreeva <e.chugreeva@gmail.com>
2022-09-22 14:12:42 +0300

    w14d2 dc

---
+++ DI_JS_pt/week14/day2/DC/carousel/src/App.js
@@ -0,0 +1,58 @@
+// import logo from './logo.svg';
+import './App.css';
+import React, { Component } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+class DemoCarousel extends Component {
+  render() {
+      return (
+      <div style = {{width:"500px", 
+      margin: "0 auto"}}>
+        <Carousel>
+              <div>
+                  <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/jrfyzvgzvhs1iylduuhj.jpg" />
+                  <p className="legend">Hong Kong</p>
+              </div>
+              <div>
+                  <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/c1cklkyp6ms02tougufx.webp" />
+                  <p className="legend">Macao</p>
+              </div>
+              <div>
+                  <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/e8fnw35p6zgusq218foj.webp" />
+                  <p className="legend">Japan</p>
+              </div>
+              <div>
+                  <img src="https://res.klook.com/image/upload/fl_lossy.progressive,q_65/c_fill,w_480,h_384/cities/liw377az16sxmp9a6ylg.webp" />
+                  <p className="legend">Las Vegas</p>
+              </div>
+          </Carousel>
+      </div>
+          
+      );
+  }
+}
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+export default DemoCarousel;
