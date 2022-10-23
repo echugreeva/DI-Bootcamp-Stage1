4da1ed52fdedeaf9db27cbe2e9101a992db8aef1
echugreeva <e.chugreeva@gmail.com>
2022-10-23 15:24:29 +0300

    w15d5 memory game

--- DI_JS_pt/week15/day5/memorygame/src/App.js
+++ DI_JS_pt/week15/day5/memorygame/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-
 import heroes from './heroes.json'
 import Card from './Card';
 
@@ -9,34 +8,40 @@ class App extends React.Component{
   constructor (){
     super();
     this.state = {
-      //object from react, you can save her whatever data you want to render
       heroesArr: heroes.superheroes,
       score:0,
       highestScore:0
     }
   }
 
-  shuffle = (arr) => {
-      let nAr = arr.sort(function () {
-        return Math.random()-0.5
-      })
-      return nAr
+  shuffle = (arr=[]) => {
+      arr.sort(()=>Math.random()-0.5)
+      return arr
   }
 
-  handleClick = (e) => {
-    console.log(`clicked on ${e.target.key}`)
-    let clickedCard = this.state.heroesArr.filter(item => (item.id === e.target.key))
-    let ind = this.state.heroesArr.indexOf(clickedCard);
+  handleClick = (id) => {
+    // console.log(this.state.heroesArr)
+    // console.log(`clicked on ${id}`);
+    let clickedCard = this.state.heroesArr.filter(item => (item.id === id))
+    // console.log(clickedCard);
+    let ind = this.state.heroesArr.indexOf(clickedCard[0]);
+    // console.log(ind);
     if(this.state.heroesArr[ind].clicked===false) {
-      let newArr = {...this.state.heroesArr};
+      let newArr = [...this.state.heroesArr];
+      console.log(`newArr: ${newArr}`)
       newArr[ind].clicked=true;
       this.setState({score: this.state.score + 1})
-      this.setState({score: this.state.highestScore + 1})
+      if(this.state.score >= this.state.highestScore) {
+        this.setState({highestScore: this.state.highestScore + 1})
+      }
+      
       this.setState({heroesArr:this.shuffle(newArr)})
       
     } else {
-      let newArr = {...this.state.heroesArr};
-      newArr = newArr.map(item => 
+      let newArr = [...this.state.heroesArr];
+      console.log(`newArr else: ${newArr}`)
+
+      newArr.map(item => 
         item.clicked = false
       )
       this.setState({heroesArr:this.shuffle(newArr)})
@@ -58,8 +63,8 @@ class App extends React.Component{
           <nav>
             <h1>Superheroes Memory Game
             </h1>
-            <p>Score:</p>
-            <p>Top Score:</p>
+            <p>Score:{this.state.score}</p>
+            <p>Top Score:{this.state.highestScore}</p>
           </nav>
           <div>
           Get points by clicking on an image but don't click on any more than once!
@@ -67,7 +72,9 @@ class App extends React.Component{
           <div> 
             {this.state.heroesArr.map((item)=>{
               return (
-                <Card key={item.id} info={item} onClick = {this.handleClick}/>
+                // <Card key={item.id} info={item} action = {()=>{console.log(item.id)}}/> this works
+
+                <Card key={item.id} info={item} onClick = {()=>{this.handleClick(item.id)}}/>
               )
               
             })}
@@ -82,6 +89,4 @@ class App extends React.Component{
 
 export default App;
 
-//game logic: every click => checj card click stus, if not clicked, change click status in card object, add score, add to highest score, reshuffle array.
-// once clicked on card that was clicked: check score vs highest score, save highest score, set score to null, set click status to false,  reshuffle 
 
