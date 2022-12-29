4b7c453b49756f448fe3b208c1e08c1e790c7639
echugreeva <e.chugreeva@gmail.com>
2022-12-29 15:28:11 +0200

    final design fixes

--- DI_JS_pt/week18/heroloweatherproject/src/components/Search.js
+++ DI_JS_pt/week18/heroloweatherproject/src/components/Search.js
@@ -1,76 +1,53 @@
-import {useContext} from 'react'
-import {AppContext} from '../App'
+import { useContext } from 'react'
+import { AppContext } from '../App'
 import AutocompleteEx from './AutoCompleteEx'
-import OneDaysEx from './OneDayWeatherEx'
-import FiveDaysEx from './FiveDaysWeatherEx.json'
 
-const Search = (props)=>{
+const Search = (props) => {
     const fetchCity = (input) => {
-    
-        // fetch(
-        //     `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=lBHawUgG9LRqS3TZNMPItFBx1VH87wIt&q=${input}`
-        //   )
-        //     .then((res) => res.json())
-        //     .then((data) => {
-        //         console.log(data);
-        //         setKeyList(data||[]);
-        //     })
-        //     .catch((err) => {
-        //         console.log(err)
-        //     })
-         setKeyList(AutocompleteEx)
+
+        fetch(
+            `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=lBHawUgG9LRqS3TZNMPItFBx1VH87wIt&q=${input}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                console.log(data);
+                setKeyList(data || []);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+        //  setKeyList(AutocompleteEx)
 
     }
 
-const fetchWeatherFive = (id) => {
-    fetch(
-        `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${id}?apikey=lBHawUgG9LRqS3TZNMPItFBx1VH87wIt&metric=true`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            setFiveDay(data.DailyForecasts);
-        })
-        .catch((err) => {
-            console.log(err)
-        })
 
 
-    // setFiveDay(FiveDaysEx.DailyForecasts)
+    const { keyList, setKeyList, chosen, setChosen } = useContext(AppContext)
 
-}
-
-    const {input,setInput} = useContext(AppContext)
-    const {keyList,setKeyList}=useContext(AppContext)
-    const {oneDay, setOneDay}=useContext(AppContext)
-    const {fiveDays, setFiveDay}=useContext(AppContext)
-    const {chosen, setChosen} =useContext(AppContext)
     return (
         <div className=' h4 flex-column items-center justify-center h-100'>
-            <input className="mv4 self-center ba br3 b--light-purple shadow-3 pa2" type='text' placeholder="Start typing city name" 
-            onChange = {(e)=>{
-                     fetchCity(e.target.value)}
-            
-            }  
-    //         {(e)=>{setInput(e.target.value)
-    //             fetchCity(input)}
-       
-    //    }  
+            <input className="w-80 w-40-m w-20-ns mv4 self-center ba br3 b--light-purple shadow-3 pa2" type='text' placeholder="Start typing city name"
+                onChange={(e) => {
+                    fetchCity(e.target.value)
+                }
+
+                }
+
             />
-            
-            <div className='mv2 w-20 center bg-white br3 '>
-        
+
+            <div className='mv2 w-80 w-20-ns center bg-white br3 '>
+
                 {
-                    keyList.map((item)=>{
+                    keyList.map((item) => {
                         return (
-                                <p className='pa1' onClick={
-                                    (e)=>{
-                                        setKeyList([])
-                                        setChosen({country:item.Country.LocalizedName, city:item.LocalizedName, key:item.Key})
-                                        // fetchWeatherFive(item.Key);
-                                    }
+                            <p className='pa1' onClick={
+                                (e) => {
+                                    setKeyList([])
+                                    setChosen({ country: item.Country.LocalizedName, city: item.LocalizedName, key: item.Key })
+                                    // fetchWeatherFive(item.Key);
                                 }
-                                key={item.Key}> {item.LocalizedName}, {item.Country.LocalizedName} </p>    
+                            }
+                                key={item.Key}> {item.LocalizedName}, {item.Country.LocalizedName} </p>
                         )
                     })
                 }
